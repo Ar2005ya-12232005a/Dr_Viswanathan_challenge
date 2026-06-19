55c796c2d87e56d20f58831d93502632d7fe860e
Arya Sankar <aryasankar16@gmail.com>
2026-06-19 22:00:45 +0530

    Day-4

---
+++ Day-4/Max_Depth_BT.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <utility>
+using namespace std;
+
+
+struct TreeNode{
+    int val;
+    
+    struct TreeNode* left;
+    struct TreeNode* right;
+
+    TreeNode(int x){
+        val = x;
+        left = right = NULL;
+    }
+};
+
+
+
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr)
+            return 0;
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        return 1 + max(leftDepth, rightDepth);
+    }
