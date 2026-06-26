cb8c3b32c8097e0476f7d2fd69f9c82b52db631f
Arya Sankar <aryasankar16@gmail.com>
2026-06-26 19:01:24 +0530

    Day-11

---
+++ Day-11/plus_one.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <utility>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        // If all digits were 9
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
