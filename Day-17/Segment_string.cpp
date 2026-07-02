15e78ebdb2bfd325c54cfdef85a1c1cf3649fc58
Arya Sankar <aryasankar16@gmail.com>
2026-07-02 20:56:57 +0530

    Day-17

---
+++ Day-17/Segment_string.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <utility>
+using namespace std;
+
+class Solution {
+public:
+    int countSegments(string s) {
+
+        int count = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            if (s[i] != ' ' && (i == 0 || s[i - 1] == ' ')) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
